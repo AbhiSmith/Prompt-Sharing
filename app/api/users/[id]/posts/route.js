e1e554aa5c62e708b3c528f6d88d544b7f86a07d
AbhiSmith <abhishekv4004@gmail.com>
2024-01-30 16:59:27 +0530

    My profile complete

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -2,5 +2,13 @@ import { connectToDb } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (request, { params }) => {
+    try {
+        await connectToDb();
 
+        const prompts = await Prompt.find({ creator: params.id }).populate('creator');
+
+        return new Response(JSON.stringify(prompts), { status: 200 });
+    } catch (error) {
+        return new Response(JSON.stringify({ message: error.message }), { status: 500 });            
+    }
 }
